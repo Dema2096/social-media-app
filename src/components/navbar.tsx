8e35c2d7cacf9a1711e9837e903e7403bdae1d1b
Matias Demarchi <mrrh96@gmail.com>
2024-01-02 05:19:55 -0300

    firebase-hooks
    added react firebase hooks

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,12 +11,10 @@ const Navbar = () =>{
         <div>
             <Link to="/">Home</Link>
             <Link to="/login">Login</Link>
-
             <div>
                 <p>{auth.currentUser?.displayName}</p>
                 <img src={auth.currentUser?.photoURL || ""} width="100" height="100"/>
             </div>
-
         </div>
     )
 
