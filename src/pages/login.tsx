d80eabea54fc1aaf70385c23b1d6eaa6ad2c802d
Matias Demarchi <mrrh96@gmail.com>
2024-01-02 05:18:26 -0300

    login
    added firebase, google auth and login function

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,24 @@
+import {auth, provider} from "../config/firebase"
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
+
+
 const Login = () =>{
+
+    const navigate = useNavigate()
+
+    const signInWithGoogle = async () =>{
+       const result = await signInWithPopup(auth,provider) 
+       console.log(result)
+       navigate("/")
+    }
+
+
     return(
-        <h1>Login Page</h1>
+        <div>
+            <p>Sign in with Google to continue</p>
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+        </div>
     )
 }
 
