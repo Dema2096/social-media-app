637a36939d7f3a9d14fa538b4f68f7d1469df74c
Matias Demarchi <mrrh96@gmail.com>
2024-01-02 05:53:13 -0300

    useAuthState/css
    added the useAuthState hook for the user profile and some css

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,23 @@
 import { Link } from "react-router-dom"
 import { auth } from "../config/firebase"
+import {useAuthState} from "react-firebase-hooks/auth"
 
 
 const Navbar = () =>{
-
+    const [user] = useAuthState(auth)
 
 
 
     return(
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <div>
-                <p>{auth.currentUser?.displayName}</p>
-                <img src={auth.currentUser?.photoURL || ""} width="100" height="100"/>
+        <div className="navbar">
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+                <div className="user">
+                    <p>{user?.displayName}</p>
+                    <img src={user?.photoURL || ""} alt="userprofilepic" width="30" height="30"/>
+                    <button>Log out</button>
+                </div>
             </div>
         </div>
     )
