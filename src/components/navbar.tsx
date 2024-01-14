a22a5423ca3e2e790614714076c8e5de0840882a
Matias Demarchi <mrrh96@gmail.com>
2024-01-14 07:20:07 -0300

    Update navbar.tsx
    added a link and changed some styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,10 +16,12 @@ const Navbar = () =>{
 
     return(
         <div className="w-full h-20  bg-cyan-800 flex justify-between">
-            <div className="flex justify-center rounded-lg p-1 m-3 bg-green-900">
-                <img src="../../5346913.png" className="h-12 w-12 "/>
-                <p className="ml-3 p-3 text-3xl font-mono">Mate Society</p>
-            </div>
+            <Link to="/">
+                <div className="flex justify-center rounded-lg p-1 mt-1.5 mb-3 ml-3 bg-green-900">
+                    <img src="../../5346913.png" className="h-12 w-12 "/>
+                    <p className="ml-3 p-3 text-3xl font-mono">Mate Society</p>
+                </div>
+            </Link>
             <div className="flex items-center justify-end">
                 <div className="mr-12 text-center text-white no-underline ">
                     <Link className="m-2.5 bg-indigo-950 p-1.5 rounded-lg" to="/">Home</Link>
