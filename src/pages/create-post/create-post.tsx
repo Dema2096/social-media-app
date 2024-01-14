07f3376349ede50eba8dd8a8e51154485a377682
Matias Demarchi <mrrh96@gmail.com>
2024-01-14 07:00:22 -0300

    Update create-post.tsx
    redirects if user hasn't logged in

--- src/pages/create-post/create-post.tsx
+++ src/pages/create-post/create-post.tsx
@@ -1,7 +1,19 @@
+import { useAuthState } from "react-firebase-hooks/auth"
 import CreateForm from "./create-form"
+import { auth } from "../../config/firebase"
+import { useNavigate } from "react-router-dom"
 
 
 const CreatePost = () =>{
+    const navigate = useNavigate()
+
+    const [user] = useAuthState(auth)
+
+    if(!user){
+        navigate("/")
+    }
+
+
     return(
         <div className="w-full h-auto flex justify-center">
             <CreateForm/>
